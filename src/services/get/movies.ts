339f26fa576bfdc279e0861f3620f196a283298c
Gina Guerrero <gnaguerrer@gmail.com>
2023-11-26 19:22:40 -0500

    feat: get movies (discover)

---
+++ src/services/get/movies.ts
@@ -0,0 +1,18 @@
+import { DefaultListParams, DefaultListResponse, MovieItem } from '@/types';
+import { axiosClient } from '..';
+
+export const getMovies = async (
+	params?: DefaultListParams
+): Promise<DefaultListResponse<MovieItem>> => {
+	try {
+		const response = await axiosClient().get('/discover/movie', { params });
+
+		if (response.status === 200) {
+			return response.data as DefaultListResponse<MovieItem>;
+		}
+		throw new Error('No movies found');
+	} catch (error) {
+		console.error('Errot at getMovies', error);
+		throw new Error('Unable to get movies');
+	}
+};
