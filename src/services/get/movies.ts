dc56e83fdd78b7c49f8e011d2818741ab2749250
Gina Guerrero <gnaguerrer@gmail.com>
2024-12-03 08:12:02 -0500

    feat: add gets for movies

--- src/services/get/movies.ts
+++ src/services/get/movies.ts
@@ -16,3 +16,37 @@ export const getMovies = async (
 		throw new Error('Unable to get movies');
 	}
 };
+
+
+export const getPopularMovies = async (
+	params?: DefaultListParams
+): Promise<DefaultListResponse<MovieItem>> => {
+	try {
+		const response = await axiosClient().get('/movie/popular', { params });
+
+		if (response.status === 200) {
+			return response.data as DefaultListResponse<MovieItem>;
+		}
+		throw new Error('No movies found');
+	} catch (error) {
+		console.error('Errot at getMovies', error);
+		throw new Error('Unable to get movies');
+	}
+};
+
+
+export const getUpcomingMovies = async (
+	params?: DefaultListParams
+): Promise<DefaultListResponse<MovieItem>> => {
+	try {
+		const response = await axiosClient().get('/movie/upcoming', { params });
+
+		if (response.status === 200) {
+			return response.data as DefaultListResponse<MovieItem>;
+		}
+		throw new Error('No movies found');
+	} catch (error) {
+		console.error('Errot at getMovies', error);
+		throw new Error('Unable to get movies');
+	}
+};
