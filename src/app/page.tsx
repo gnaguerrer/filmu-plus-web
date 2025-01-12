1b73178f5cb851b043c62ff1feb331522244baba
Gina Guerrero <gnaguerrer@gmail.com>
2025-01-12 11:50:44 -0500

    add posters in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,31 @@
 import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { PosterList } from '@/components';
-import { getMovies } from '@/services';
+import { getPopularMovies, getTrending } from '@/services';
 
 const HomePage = (): React.JSX.Element => {
-	const { data, isLoading } = useQuery({
-		queryKey: ['MOVIES'],
-		queryFn: () => getMovies(),
+	const { data: popular, isLoading: isLoadingPopular } = useQuery({
+		queryKey: ['POPULAR_MOVIES'],
+		queryFn: () => getPopularMovies(),
+	});
+
+	const { data: trending, isLoading: isLoadingTrending } = useQuery({
+		queryKey: ['TRENDING_DAY'],
+		queryFn: () => getTrending('day'),
 	});
 
 	return (
 		<main className="flex flex-col flex-grow mt-16 scrollbar-dark overflow-x-hidden max-w-full gap-4">
 			<PosterList
-				data={data?.results ?? []}
-				isLoading={isLoading}
-				title="Estrenos"
+				data={trending?.results ?? []}
+				isLoading={isLoadingTrending}
+				title="Trending Today"
 			/>
-			{/* <PosterList data={data?.results ?? []} isLoading={isLoading} /> */}
+			<PosterList
+				data={popular?.results ?? []}
+				isLoading={isLoadingPopular}
+				title="Popular Movies"
+			/>
 		</main>
 	);
 };
